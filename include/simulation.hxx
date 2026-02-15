154d8c882a0fa386cd75e9b28fa97c35610867b6
mapilogo <lezigiuszdicaprio@gmail.com>
2026-02-15 21:01:18 +0100

    Update simulation.hxx
    Poprawiona logika IntervalReportNotifier

--- include/simulation.hxx
+++ include/simulation.hxx
@@ -1,26 +1,26 @@
-#ifndef SIMULATION_HXX
-#define SIMULATION_HXX
+#ifndef NETSIM_SIMULATION_HXX
+#define NETSIM_SIMULATION_HXX
 
 #include "factory.hxx"
+#include <set>
+#include <functional>
 
 class IntervalReportNotifier {
+public:
+    explicit IntervalReportNotifier(TimeOffset to) : to_(to) {}
+    bool should_generate_report(Time t) const { return t == 1 || t % to_ == 1; }
 private:
     TimeOffset to_;
-public:
-    explicit IntervalReportNotifier(TimeOffset to) : to_(to) {};
-
-    bool should_generate_report(Time t) { return t == 1 || t % to_ == 1; };
 };
 
 class SpecificTurnsReportNotifier {
+public:
+    explicit SpecificTurnsReportNotifier(const std::set<Time>& turns) : turns_(turns) {}
+    bool should_generate_report(Time t) const { return turns_.find(t) != turns_.end(); }
 private:
     std::set<Time> turns_;
-public:
-    explicit SpecificTurnsReportNotifier(const std::set<Time>& turns) : turns_(turns) {};
-
-    bool should_generate_report(Time t) { return turns_.find(t) != turns_.cend(); };
 };
 
 void simulate(Factory& factory, TimeOffset d, std::function<void(Factory&, TimeOffset)>&& rf);
 
-#endif //SIMULATION_HXX
+#endif
